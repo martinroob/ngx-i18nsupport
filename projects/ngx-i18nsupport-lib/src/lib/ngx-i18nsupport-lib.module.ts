78759a9b0190747a7e4ce72d6e2affa33ce97a1e
Martin Roob <martinroob65@gmail.com>
2018-10-26 09:07:36 +0200

    ng generate library ngx-i18nsupport-lib

---
+++ projects/ngx-i18nsupport-lib/src/lib/ngx-i18nsupport-lib.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { NgxI18nsupportLibComponent } from './ngx-i18nsupport-lib.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [NgxI18nsupportLibComponent],
+  exports: [NgxI18nsupportLibComponent]
+})
+export class NgxI18nsupportLibModule { }
---
+++ projects/ngx-i18nsupport-lib/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-i18nsupport-lib
+ */
+
+export * from './lib/ngx-i18nsupport-lib.service';
+export * from './lib/ngx-i18nsupport-lib.component';
+export * from './lib/ngx-i18nsupport-lib.module';
